302aac631e66e8196deb1e9b73c107f97089365c
Sergei Ermolaev <sayermo4@mts.ru>
2020-04-12 19:54:05 +0300

    fix gradle dsl assembling

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 @file:Suppress("SpellCheckingInspection")
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
 
     repositories {
@@ -12,12 +10,9 @@ buildscript {
     dependencies {
         classpath("com.google.gms:google-services:4.3.3")
         classpath("com.android.tools.build:gradle:3.6.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$Versions.kotlin")
+        classpath(kotlin("gradle-plugin", version = Versions.kotlin))
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.1.1")
         classpath("org.jacoco:org.jacoco.core:0.8.5")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 

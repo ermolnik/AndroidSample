37969a54c8f642c3cf5de7c9b03b4af7bcc778f3
Sergei Ermolaev <sayermo4@mts.ru>
2020-05-05 22:03:55 +0300

    has been added stubbed features and data modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "AndroidSample"
 include(":app")
 include(":buildSrc")
 include(":base")
+include(":data")
+include(":features:auth")
+include(":features:main")
+include(":features:settings")

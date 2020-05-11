91e01b7b5acbef1d041f4e20835ab3946e53a760
Sergei Ermolaev <sayermo4@mts.ru>
2020-05-11 20:05:42 +0300

    added debug type to base module

--- core/base/src/main/java/ru/ermolnik/base/util/TimberFactory.kt
+++ core/base/src/main/java/ru/ermolnik/base/util/TimberFactory.kt
@@ -1,12 +1,13 @@
 package ru.ermolnik.base.util
 
+import ru.ermolnik.base.BuildConfig.DEBUG
 import timber.log.Timber
 
 object TimberFactory {
     fun setupOnDebug() {
         Timber.uprootAll()
-//        if (DEBUG) {
+        if (DEBUG) {
             Timber.plant(Timber.DebugTree())
-//        }
+        }
     }
 }

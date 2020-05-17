2359dc23cc764db114f8a13ae837792d57076f48
Sergei Ermolaev <ermolnik0@mail.ru>
2020-05-17 21:31:56 +0300

    test contributing graph

--- app/src/main/java/ru/ermolnik/androidsample/vm/ViewModelKey.kt
+++ app/src/main/java/ru/ermolnik/androidsample/vm/ViewModelKey.kt
@@ -12,3 +12,4 @@ import kotlin.reflect.KClass
 @Target(FUNCTION, PROPERTY_GETTER, PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ViewModelKey(val value: KClass<out ViewModel>)
+
